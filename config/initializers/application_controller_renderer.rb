205d8ed173caa10c3fd77537bfec9de8cfe4ec04
vagrant <vagrant@localhost.localdomain>
2020-04-05 10:00:26 +0000

    [update-favorite-Implementation]rails-application-2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c2978258f261bba8e37757bcc709e991f45e19fa77f78954c96eb4cd8a9e84c66e488c682244b85532b6e1bb92c3d2139ca9603975633b345d9ddc0b1fe14bc'
